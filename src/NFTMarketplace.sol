343a0bf0cb29037ae95438847836236bcfcc5a59
usman-qsols <usmanrahim.qsols@gmail.com>
2023-08-31 11:02:10 +0500

    dependencies

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin-contracts/contracts/utils/Counters.sol";
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 
 
@@ -17,7 +17,7 @@ contract Marketplace is Ownable,ReentrancyGuard{
     struct MarketItem{
         uint256 tokenId;
         address seller;
-        address tokenContract;
+        address nftContractAddress;
         uint256 price;
         bool active;
     }
@@ -25,13 +25,13 @@ contract Marketplace is Ownable,ReentrancyGuard{
     mapping(uint256 =>mapping (address => MarketItem)) public idToMarketItem;
     mapping (uint256=> mapping (address => address)) public ownerOfItem;
 
-    function createListing(address _tokenContract, uint256 _tokenId, uint256 _price) external payable nonReentrant  {
+    function listNft(address _nftContractAddress, uint256 _tokenId, uint256 _price) external payable nonReentrant  {
         require(msg.value >= listingPrice,"Insufficient Balance");
-        require(IERC721(_tokenContract).ownerOf(_tokenId) == msg.sender, "You don't own this token");
-        IERC721(_tokenContract).transferFrom(msg.sender, address(this), _tokenId);
+        require(IERC721(_nftContractAddress).ownerOf(_tokenId) == msg.sender, "You don't own this token");
+        IERC721(_nftContractAddress).transferFrom(msg.sender, address(this), _tokenId);
         listCounts.increment();
-        idToMarketItem[_tokenId][_tokenContract] = MarketItem(_tokenId, msg.sender,_tokenContract, _price, true );
-        ownerOfItem[_tokenId][_tokenContract] = msg.sender;
+        idToMarketItem[_tokenId][_nftContractAddress] = MarketItem(_tokenId, msg.sender,_nftContractAddress, _price, true );
+        ownerOfItem[_tokenId][_nftContractAddress] = msg.sender;
         if(msg.value > listingPrice){
             uint256 remainingAmount = msg.value - listingPrice;
             (bool sent,) = msg.sender.call{value : remainingAmount}("");
@@ -39,18 +39,18 @@ contract Marketplace is Ownable,ReentrancyGuard{
         }
     }
 
-    function purchaseItem(uint256 tokenId, address _tokenContract) external payable nonReentrant{
-        require(idToMarketItem[tokenId][_tokenContract].active== true, "Listing is not active");
-        require(msg.value>= idToMarketItem[tokenId][_tokenContract].price,"Insufficient funds");
-        (bool sent,) = payable(idToMarketItem[tokenId][_tokenContract].seller).call{value : msg.value}("");
+    function purchaseNft(uint256 tokenId, address _nftContractAddress) external payable nonReentrant{
+        require(idToMarketItem[tokenId][_nftContractAddress].active== true, "Listing is not active");
+        require(msg.value>= idToMarketItem[tokenId][_nftContractAddress].price,"Insufficient funds");
+        (bool sent,) = payable(idToMarketItem[tokenId][_nftContractAddress].seller).call{value : msg.value}("");
         require(sent,"Ethers Can't sent");
-        IERC721(idToMarketItem[tokenId][_tokenContract].tokenContract).transferFrom( address(this), msg.sender, tokenId);
-        idToMarketItem[tokenId][_tokenContract].active = false;
-        idToMarketItem[tokenId][_tokenContract].seller = msg.sender;
-        ownerOfItem[tokenId][_tokenContract] = msg.sender;
+        IERC721(idToMarketItem[tokenId][_nftContractAddress].nftContractAddress).transferFrom( address(this), msg.sender, tokenId);
+        idToMarketItem[tokenId][_nftContractAddress].active = false;
+        idToMarketItem[tokenId][_nftContractAddress].seller = msg.sender;
+        ownerOfItem[tokenId][_nftContractAddress] = msg.sender;
     }
 
-    function withdraw() public onlyOwner nonReentrant{
+    function withdrawAmount() public onlyOwner nonReentrant{
         (bool sent,) = payable(msg.sender).call{value : address(this).balance}("");
         require(sent,"Ethers Can't sent");
     }
