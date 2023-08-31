6bf29961fe2f40c2af8a8f5fbf4747c94a622470
usman-qsols <usmanrahim.qsols@gmail.com>
2023-08-31 13:38:45 +0500

    added test cases

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,6 +1,6 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+
 import "@openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin-contracts/contracts/utils/Counters.sol";
@@ -11,7 +11,8 @@ import "@openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 /// @notice A smart contract for listing and purchasing NFTs on a marketplace
 contract Marketplace is Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
-    Counters.Counter private listCounts;
+
+    Counters.Counter public listCounts;
     uint256 public listingPrice = 0.0025 ether;
 
     /// @title MarketItem
@@ -23,6 +24,9 @@ contract Marketplace is Ownable, ReentrancyGuard {
         uint256 price; // Represents the price of the NFT in wei.
         bool active; // Represents whether the listing is active or not.
     }
+
+    event listingNft(address nftContractAddress , uint256 tokenId , uint256 price);
+    event purchasedNft(uint256 tokenId, address buyer, uint256 price);
 
     mapping(uint256 => mapping(address => MarketItem)) public idToMarketItem;
     mapping(uint256 => mapping(address => address)) public ownerOfItem;
@@ -36,13 +40,15 @@ contract Marketplace is Ownable, ReentrancyGuard {
         require(IERC721(_nftContractAddress).ownerOf(_tokenId) == msg.sender, "You don't own this token");
         IERC721(_nftContractAddress).transferFrom(msg.sender, address(this), _tokenId);
         listCounts.increment();
-        idToMarketItem[_tokenId][_nftContractAddress] = MarketItem(_tokenId, msg.sender, _nftContractAddress, _price, true);
+        idToMarketItem[_tokenId][_nftContractAddress] =
+            MarketItem(_tokenId, msg.sender, _nftContractAddress, _price, true);
         ownerOfItem[_tokenId][_nftContractAddress] = msg.sender;
         if (msg.value > listingPrice) {
             uint256 remainingAmount = msg.value - listingPrice;
             (bool sent,) = msg.sender.call{value: remainingAmount}("");
             require(sent, "Ethers Can't sent");
         }
+        emit listingNft(_nftContractAddress,_tokenId,_price);
     }
 
     /// @notice Purchases an NFT from the marketplace
@@ -53,10 +59,20 @@ contract Marketplace is Ownable, ReentrancyGuard {
         require(msg.value >= idToMarketItem[tokenId][_nftContractAddress].price, "Insufficient funds");
         (bool sent,) = payable(idToMarketItem[tokenId][_nftContractAddress].seller).call{value: msg.value}("");
         require(sent, "Ethers Can't sent");
-        IERC721(idToMarketItem[tokenId][_nftContractAddress].nftContractAddress).transferFrom(address(this), msg.sender, tokenId);
+        IERC721(idToMarketItem[tokenId][_nftContractAddress].nftContractAddress).transferFrom(
+            address(this), msg.sender, tokenId
+        );
+        // if (msg.value > idToMarketItem[tokenId][_nftContractAddress].price) {
+        //     uint256 remainingAmount = msg.value - idToMarketItem[tokenId][_nftContractAddress].price;
+        //     (bool sentRemainingAmount,) = msg.sender.call{value: remainingAmount}("");
+        //     require(sentRemainingAmount, "Ethers Can't sent");
+        // }
+        listCounts.decrement();
+
         idToMarketItem[tokenId][_nftContractAddress].active = false;
         idToMarketItem[tokenId][_nftContractAddress].seller = msg.sender;
         ownerOfItem[tokenId][_nftContractAddress] = msg.sender;
+        emit purchasedNft(tokenId,msg.sender, idToMarketItem[tokenId][_nftContractAddress].price);
     }
 
     /// @notice Allows the contract owner to withdraw the balance of the contract
--- test/NFTMarketplace.t.sol
+++ test/NFTMarketplace.t.sol
@@ -1,13 +1,77 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {console,Test} from "forge-std/Test.sol";
+import {console, Test} from "forge-std/Test.sol";
 import "../src/NFTMarketplace.sol";
+import "../src/ERC721contract.sol";
 
-contract MarketplaceTestContract is Test{
+contract MarketplaceTestContract is Test {
     Marketplace marketplace;
+    MyToken nft;
 
-    function setUp() public{
+    function setUp() public {
         marketplace = new Marketplace();
+        nft = new MyToken();
+    }
+
+    function testMintNft() public {
+        nft.safeMint(address(1),1,"audi");
+        assertEq(nft.balanceOf(address(1)),1);
+        console.log(address(this));
+        console.log(address(nft));
+        console.log(address(marketplace));
+    }
+
+    function testListingNft() public{
+        nft.safeMint(address(1),1,"audi");
+        vm.deal(address(1),2 ether);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(nft),true);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(marketplace),true);
+        vm.prank(address(1));
+        marketplace.listNft{value : 1 ether}(0x2e234DAe75C793f67A35089C9d99245E1C58470b,1,1);
+        assertEq(marketplace.listCounts(),1);
+        
+    }
+
+    function testPurchaseNft() public{
+        nft.safeMint(address(1),1,"audi");
+        vm.deal(address(1),2 ether);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(nft),true);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(marketplace),true);
+        vm.prank(address(1));
+        marketplace.listNft{value : 1 ether}(0x2e234DAe75C793f67A35089C9d99245E1C58470b,1,1);
+        assertEq(marketplace.listCounts(),1);
+
+        vm.deal(address(2),2 ether);
+        vm.prank(address(2));
+        marketplace.purchaseNft{value : 1 ether}(1,address(nft));
+        assertEq(nft.balanceOf(address(2)),1);
+    }
+
+    function testResellNft() public{
+        nft.safeMint(address(1),1,"audi");
+        vm.deal(address(1),2 ether);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(nft),true);
+        vm.prank(address(1));
+        nft.setApprovalForAll(address(marketplace),true);
+        vm.prank(address(1));
+        marketplace.listNft{value : 1 ether}(0x2e234DAe75C793f67A35089C9d99245E1C58470b,1,1);
+        assertEq(marketplace.listCounts(),1);
+
+        vm.deal(address(2),2 ether);
+        vm.prank(address(2));
+        marketplace.purchaseNft{value : 1 ether}(1,address(nft));
+        assertEq(nft.balanceOf(address(2)),1);
+
+        vm.prank(address(2));
+        nft.setApprovalForAll(address(marketplace),true);
+        vm.prank(address(2));
+        marketplace.listNft{value : 1 ether}(0x2e234DAe75C793f67A35089C9d99245E1C58470b,1,1);
+        assertEq(marketplace.listCounts(),1);
     }
 }
